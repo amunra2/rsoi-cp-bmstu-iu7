9d0b14e5ceab74b7625acd76fed67cc68c870dc9
amunra2 <ivancvet2001@gmail.com>
2024-09-11 14:34:54 +0300

    feat(): Указание названия бд

--- src/backend/db_sql/10-create-databases.sh
+++ src/backend/db_sql/10-create-databases.sh
@@ -4,8 +4,8 @@ set -e
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=password
 psql -U postgres -f "$SCRIPT_PATH/sql/db-create.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_auth.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_libraries.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_ratings.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_reservations.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_statistics.sql"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_auth.sql auth"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_libraries.sql libraries"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_ratings.sql ratings"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_reservations.sql reservations"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_statistics.sql statistics"
