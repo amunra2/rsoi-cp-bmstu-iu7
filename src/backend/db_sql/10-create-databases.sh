b7fc8a03d4339f9b738ed2997db887bb9cf21312
amunra2 <ivancvet2001@gmail.com>
2024-09-11 14:47:37 +0300

    fix(): Имена бд

--- src/backend/db_sql/10-create-databases.sh
+++ src/backend/db_sql/10-create-databases.sh
@@ -4,8 +4,8 @@ set -e
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=password
 psql -U postgres -f "$SCRIPT_PATH/sql/db-create.sql"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_auth.sql auth"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_libraries.sql libraries"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_ratings.sql ratings"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_reservations.sql reservations"
-psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_statistics.sql statistics"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_auth.sql -d auth"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_libraries.sql -d libraries"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_ratings.sql -d ratings"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_reservations.sql -d reservations"
+psql -U postgres -f "$SCRIPT_PATH/sql/db/dump_statistics.sql -d statistics"
